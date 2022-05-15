f104e71f1b3318c0d45abd229ecc01490a66ebbd
NeoScript <neoscripting@gmail.com>
2022-05-15 05:09:38 +0000

    remove old testing pages
    it is time to start taking advantage of angular components

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -4,11 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TopicsComponent } from './components/topics/topics.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { IndexComponent } from './components/index/index.component';
-import { SubscriptionsComponent } from './components/subscriptions/subscriptions.component';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -22,9 +20,7 @@ import { TopicListComponent } from './components/topic-list/topic-list.component
 @NgModule({
   declarations: [
     AppComponent,
-    TopicsComponent,
     IndexComponent,
-    SubscriptionsComponent,
     ProjectsComponent,
     NavbarComponent,
     TopicListComponent
