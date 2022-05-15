1151c29360db68746beaf34bfd205930128b71bf
NeoScript <neoscripting@gmail.com>
2022-05-15 02:17:45 +0000

    styling on topic page for basic layout

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
@@ -36,6 +37,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     MatCardModule,
     MatIconModule,
     MatToolbarModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- webapp/src/app/components/projects/projects.component.ts
+++ webapp/src/app/components/projects/projects.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { PubsubService } from 'src/app/services/pubsub.service';
+import { EMPTY, firstValueFrom, Observable, of, switchMap } from 'rxjs';
+import { PubsubService, Topic } from 'src/app/services/pubsub.service';
 
 @Component({
   selector: 'app-projects',
@@ -10,6 +11,10 @@ import { PubsubService } from 'src/app/services/pubsub.service';
 export class ProjectsComponent implements OnInit {
 
   currentProject: string = ''
+
+  topicList$: Observable<Topic[]> = EMPTY
+
+  currentTopic?: Topic
   constructor(private route: ActivatedRoute, private pubsub: PubsubService) { }
 
   ngOnInit(): void {
@@ -17,7 +22,26 @@ export class ProjectsComponent implements OnInit {
       this.currentProject = qpm.get("project") ?? ''
 
       console.log("loaded project: ", this.currentProject)
+      this.pubsub.listTopics(this.currentProject)
+        .pipe(
+          switchMap(async topicList => {
+            for await (const topic of topicList) {
+              topic.subscriptions = await firstValueFrom(this.pubsub.listSubscriptionsOnTopic(topic.name))
+              console.log(topic.subscriptions)
+            }
+
+            console.log(topicList)
+            return of(topicList)
+          }))
+        .subscribe(result => {
+          this.topicList$ = result
+        })
     })
+  }
+
+
+  selectTopic(topic: Topic) {
+    this.currentTopic = topic
   }
 
 }
