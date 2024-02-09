e6ae138550428fad378931b773fa559b8da8f4a7
neoscript <neoscripting@gmail.com>
2024-02-09 03:11:18 +0000

    Create ability to add project names

---
+++ webapp/src/app/components/input-dialog/input-dialog.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InputDialogComponent } from './input-dialog.component';
+
+describe('InputDialogComponent', () => {
+  let component: InputDialogComponent;
+  let fixture: ComponentFixture<InputDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [InputDialogComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(InputDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ webapp/src/app/components/input-dialog/input-dialog.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-input-dialog',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, MatInputModule, ReactiveFormsModule, MatFormFieldModule],
+  templateUrl: './input-dialog.component.html',
+  styleUrl: './input-dialog.component.scss'
+})
+export class InputDialogComponent {
+
+  input = new FormControl('', Validators.required)
+
+  constructor(private dialogRef: MatDialogRef<InputDialogComponent>){
+
+  }
+
+  save(){
+    this.dialogRef.close({project_id: this.input.value})
+  }
+
+}
