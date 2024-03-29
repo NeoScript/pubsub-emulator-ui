b3d8569080609bda3966a1b143f100c3d0149cea
alihangokdemir <alihangokdemir@gmail.com>
2024-03-29 16:52:18 +0100

    add attributes field

--- webapp/src/app/components/topic-details/topic-details.component.ts
+++ webapp/src/app/components/topic-details/topic-details.component.ts
@@ -1,29 +1,29 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { UntypedFormControl, Validators } from '@angular/forms';
 import { Topic } from 'src/app/services/pubsub.service';
-
 @Component({
   selector: 'app-topic-details',
   templateUrl: './topic-details.component.html',
   styleUrls: ['./topic-details.component.scss']
 })
 export class TopicDetailsComponent implements OnInit {
-
   @Input() topic?: Topic
-
-  @Output() onMessagePublish = new EventEmitter<{ topic: Topic, message: string }>()
-
+  @Output() onMessagePublish = new EventEmitter<{ topic: Topic, message: string, attributes: { [key: string]: string } }>()
   public inputField = new UntypedFormControl('', Validators.required)
-  constructor() { }
+  public attributesField = new UntypedFormControl('', Validators.required)
 
+  constructor() { }
   ngOnInit(): void {
   }
-
   publishMessage() {
     console.log("this value was found", this.inputField.value)
 
-    this.onMessagePublish.emit({ topic: this.topic!, message: this.inputField.value })
+    let attributes = null;
+    if (this.attributesField.value != "") {
+      attributes = JSON.parse(this.attributesField.value)
+    }
+    this.onMessagePublish.emit({ topic: this.topic!, message: this.inputField.value, attributes: attributes })
     this.inputField.reset()
+    this.attributesField.reset()
   }
-
 }
