2170bcfc3afe9043a5ade892fd8f08c266e8576f
neoscript <neoscripting@gmail.com>
2024-10-01 21:39:27 -0500

    3rd step migrate to standalone 2nd step had no changes

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [NavbarComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'webapp';
