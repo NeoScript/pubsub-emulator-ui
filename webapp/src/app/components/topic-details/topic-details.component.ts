c0bcbf2044640b8aaf9d1836c6b615082625f506
neoscript <neoscripting@gmail.com>
2025-01-15 05:44:59 +0000

    organize imports

--- webapp/src/app/components/topic-details/topic-details.component.ts
+++ webapp/src/app/components/topic-details/topic-details.component.ts
@@ -1,12 +1,12 @@
-import { Component, computed, EventEmitter, inject, Input, OnInit, Output, signal } from '@angular/core';
-import { UntypedFormControl, Validators, ReactiveFormsModule } from '@angular/forms';
-import { Topic } from 'src/app/services/pubsub.service';
-import { MatButton } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
-import { MatFormField } from '@angular/material/form-field';
-import { MatInput } from '@angular/material/input';
 import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
+import { ReactiveFormsModule, UntypedFormControl, Validators } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
+import { MatFormField } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
+import { Topic } from 'src/app/services/pubsub.service';
 import { AttributeEditorComponent } from './attribute-editor/attribute-editor.component';
 
 @Component({
